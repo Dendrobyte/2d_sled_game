91a1d0011e07cba1a06aca7ccda9c9ef05676d51
Dendrobyte <markbacon78@gmail.com>
2024-06-28 23:38:15 -0700

    Lil ski man moves around

---
+++ char.lua
@@ -0,0 +1,34 @@
+-- Define the character as a table I guess
+local char = {}
+
+function char.load()
+    char.name = "Mark"
+    char.move_one = love.graphics.newImage("ski_assets/Tiles/tile_0082.png")
+    char.move_two = love.graphics.newImage("ski_assets/Tiles/tile_0083.png")
+    char.sprite = char.move_one
+
+    char.x = 400
+    char.y = 100
+end
+
+local count = 0
+function char.update_sprite(dt)
+    -- TODO: Something something framerate independent?
+
+    -- Simulate downard skiing
+    if count % 2 == 0 then
+        char.y = char.y + 1
+    end
+
+    -- Swap sprites back and forth to simulate skiing motions
+    if count == 40 then
+        char.sprite = char.move_one
+    end
+    if count == 80 then
+        char.sprite = char.move_two
+        count = -1
+    end
+    count = count + 1
+end
+
+return char
--- main.lua
+++ main.lua
@@ -1,12 +1,34 @@
+local char = require("char")
+
+-- Load default values
 function love.load()
-    frame = 0
+    -- Set up some default behaviors
+    love.graphics.setDefaultFilter("nearest", "nearest", 0) -- Linear can die :)
+    love.keyboard.setKeyRepeat(true)
+
+    -- Load "objects"
+    char.load() 
 end
 
+function love.update(dt)
+    char.update_sprite(dt)
+end
+
+-- Draw a coloured rectangle.
 function love.draw()
-    sec = math.floor(frame/60)
-    love.graphics.print("Current Sec: " .. sec .. "| Current Frame: " .. frame)
+    -- In versions prior to 11.0, color component values are (0, 102, 102)
+    love.graphics.print("Hello World! Debug text?", 800, 50)
+    love.graphics.draw(char.sprite, char.x, char.y, 0, 3)
 end
 
-function love.update()
-    frame = frame + 1
+function love.keypressed(key, isrepeat)
+    if key == "right" or key == "d" then
+        char.x = char.x + 10
+    end
+    if key == "left" or key == "a" then
+        char.x = char.x - 10
+    end
+    if key == "down" or key == "s" then
+        char.y = char.y + 5 -- TODO: Lerp this...? I think that's the animation keyword.
+    end
 end
